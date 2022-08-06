ac1a742ef1eaf6202b447d71b26467116729cf95
deepjyotiA <gouravdeb@gmail.com>
2022-08-06 19:15:27 +0530

    run load commit

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import TableContainer from '../table-container/TableContainer'
 
 export default function Home() {
+  console.log('run on load')
   return (
     <div>
         <TableContainer/>
